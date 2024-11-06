c32a4152f3d948ea81698a3c91a7a64a26aaefad
Comp <Comp@PENDULUM>
2024-11-06 06:55:11 +0500

    Добавьте файлы проекта.

---
+++ Point.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace амогус
+{
+    public class Point
+    {
+
+        private double x;
+        private double y;
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+
+        }
+        public Point(Point parents)
+        {
+            x = parents.x;
+            y = parents.y;
+        }
+        public string Tostring()
+        {
+            string s = $"{x}, {y}";
+            return s;
+        }
+        public override string ToString()
+        {
+            base.ToString();
+            string s = $"{x} , {y}";
+            return s;
+        }
+        public double Range()
+        {
+            return (Math.Abs(x - y));
+        }
+        public static Point operator ++(Point a)
+        {
+            return new Point(a.x++, a.y);
+        }
+        public static Point operator --(Point a)
+        {
+
+            return new Point(a.x--, a.y);
+        }
+        public static explicit operator int(Point a)
+        {
+            return (int)a.x;
+        }
+        public static implicit operator double(Point a)
+        {
+
+            double z = a.y;
+            return z;
+        }
+        public static double operator +(Point One, Point Two)
+        {
+            double a = Math.Sqrt((One.x - Two.x) * (One.x - Two.x) + (One.y - Two.y) * (One.y - Two.y));
+            return a;
+        }
+        public static Point operator +(Point One, double q)
+        {
+            Point New = new Point(One.x + q, One.y);
+            return New;
+        }
+        public static Point operator +(double q, Point One)
+        {
+            Point New = new Point(One.x + q, One.y);
+            return New;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+namespace амогус
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x, y;
+            double x1, y1;
+            Console.WriteLine("Введите x");
+            x = Console.ReadLine();
+            try
+            {
+                x1 = Convert.ToDouble(x);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("x Не является числом");
+                throw;
+            }
+            Console.WriteLine("Введите y");
+            y = Console.ReadLine();
+            try
+            {
+                y1 = Convert.ToDouble(y);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("y Не является числом");
+                throw;
+            }
+            Point One=new Point(x1,y1);
+            Point Two = new Point(One);
+            Console.Write("X и Y в виде строки: ");
+            string s = Two.ToString();
+            Console.WriteLine(s);
+            Console.Write("Расстояние от X до Y: ");
+            double dis = Two.Range();
+            Console.WriteLine(dis);
+            Console.Write("Бинарный +: ");
+            Two = Two++;
+            Console.WriteLine(Two.ToString());
+            Console.Write("Бинарный -: ");
+            Two = Two--;
+            Console.WriteLine(Two.ToString());
+            Console.Write("Явное приведение: ");
+            Console.WriteLine((int)Two);
+            Console.Write("Неявное приведение: ");
+            double z = Two;
+            Console.WriteLine(z);
+            double x2, y2;
+            Console.WriteLine("Введите x точки p");
+            x = Console.ReadLine();
+            try
+            {
+                x2 = Convert.ToDouble(x);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("x Не является числом");
+                throw;
+            }
+            Console.WriteLine("Введите y точки p");
+            y = Console.ReadLine();
+            try
+            {
+                y2 = Convert.ToDouble(y);
+            }
+            catch(Exception ex )
+            {
+                Console.WriteLine("y Не является числом");
+                throw;
+            }
+            Point Three = new Point(x2,y2);
+            double g=One+Three;
+            Console.Write("Расстояние между XY и pXpY: ");
+            Console.WriteLine(g);
+
+            Point Some=One+x2;
+            Console.Write("Добавление к точке XY координаты pX справа: ");
+            Console.WriteLine(Some.ToString());
+            Point Some2 = x2+One;
+            Console.Write("Добавление к точке XY координаты pX слева: ");
+            Console.WriteLine(Some2.ToString());
+        }
+
+    }
+}
